219186e1ee0d1b56f6708f1310853d3a9d117dd3
rodolfom9 <rodolfomartins09@gmail.com>
2025-10-04 22:47:53 -0300

    1.0.0 Plugin Minimo Funcional

---
+++ i18n/rmcgeo_pt.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>RMCGeo</name>
+    <message>
+        <source>RMCGEO</source>
+        <translation>RMCGEO</translation>
+    </message>
+    <message>
+        <source>Memorial Descritivo (In Development)</source>
+        <translation>Memorial Descritivo (Em Desenvolvimento)</translation>
+    </message>
+    <message>
+        <source>Applications</source>
+        <translation>Aplicações</translation>
+    </message>
+    <message>
+        <source>Drawing by Azimuth and Distance</source>
+        <translation>Desenho por Azimute e Distância</translation>
+    </message>
+    <message>
+        <source>Decimal to GMS Converter</source>
+        <translation>Conversor de Decimal para GMS</translation>
+    </message>
+    <message>
+        <source>Tools</source>
+        <translation>Ferramentas</translation>
+    </message>
+    <message>
+        <source>Copy Coordinates</source>
+        <translation>Copiar Coordenadas</translation>
+    </message>
+    <message>
+        <source>About RMCGEO</source>
+        <translation>Sobre o RMCGEO</translation>
+    </message>
+</context>
+</TS>
